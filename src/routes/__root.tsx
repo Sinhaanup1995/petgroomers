7ad739bd6317ab78cb719a0d0585a6b1d32633e6
Sinhaanup1995 <sinhaanup65@gmail.com>
2026-09-03 11:47:17 +0530

    gtm update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -107,6 +107,20 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17957354748"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17957354748');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
